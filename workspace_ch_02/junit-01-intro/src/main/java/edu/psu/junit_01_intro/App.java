da39136696c77b7156b582e93f627f139f78d844
Skander Turki <skanderturki@gmail.com>
2025-01-01 22:24:13 +0300

    added junit example

---
+++ workspace_ch_02/junit-01-intro/src/main/java/edu/psu/junit_01_intro/App.java
@@ -0,0 +1,10 @@
+package edu.psu.junit_01_intro;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ workspace_ch_02/junit-01-intro/src/main/java/edu/psu/junit_01_intro/Util.java
@@ -0,0 +1,9 @@
+package edu.psu.junit_01_intro;
+
+public class Util {
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+}
